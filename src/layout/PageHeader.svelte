63781474868be94090672c2292e27dfb2a5c2b5e
EriksonGM <erik.melgarejo@hotmail.com>
2023-10-25 16:17:39 +0100

    Init

---
+++ src/layout/PageHeader.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let Title = "";
+    export let Icon ="";
+</script>
+
+<h2><i class="{Icon}"></i>{Title}</h2>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,101 @@
+<script>
+    import Footer from "../layout/Footer.svelte";
+    import UserProfile from "../layout/UserProfile.svelte";
+    import NavBar from "../layout/NavBar.svelte";
+</script>
+
+<svelte:head>
+    <link href="/css/light/loader.css" rel="stylesheet" type="text/css"/>
+    <link href="/css/dark/loader.css" rel="stylesheet" type="text/css"/>
+    <link href="fonts/tablericon/tabler-icons.css" type="text/css">
+    <script src="/js/loader.js"></script>
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+
+    <link href="/libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+
+    <link href="/css/light/plugins.css" rel="stylesheet" type="text/css"/>
+    <link href="/css/dark/plugins.css" rel="stylesheet" type="text/css"/>
+
+    <script src="/libs/global/vendors.min.js"></script>
+    <script src="/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/libs/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="/js/app.js"></script>
+
+    <script src="/js/custom.js"></script>
+</svelte:head>
+
+<!--  BEGIN NAVBAR  -->
+<div class="header-container container-xxl">
+    <header class="header navbar navbar-expand-sm expand-header">
+        <a href="/" class="nav-link theme-toggle p-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 class="feather feather-moon dark-mode">
+                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 class="feather feather-sun light-mode">
+                <circle cx="12" cy="12" r="5"></circle>
+                <line x1="12" y1="1" x2="12" y2="3"></line>
+                <line x1="12" y1="21" x2="12" y2="23"></line>
+                <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+                <line x1="1" y1="12" x2="3" y2="12"></line>
+                <line x1="21" y1="12" x2="23" y2="12"></line>
+                <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+            </svg>
+        </a>
+
+        <a href="/" class="sidebarCollapse">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 class="feather feather-menu">
+                <line x1="3" y1="12" x2="21" y2="12"></line>
+                <line x1="3" y1="6" x2="21" y2="6"></line>
+                <line x1="3" y1="18" x2="21" y2="18"></line>
+            </svg>
+        </a>
+        <ul class="navbar-item flex-row ms-lg-auto ms-0">
+            <UserProfile/>
+        </ul>
+    </header>
+</div>
+<!--  END NAVBAR  -->
+
+<!--  BEGIN MAIN CONTAINER  -->
+<div class="main-container" id="container">
+    <div class="overlay"></div>
+    <div class="search-overlay"></div>
+    <!--  BEGIN SIDEBAR  -->
+    <NavBar/>
+    <!--  END SIDEBAR  -->
+
+    <!--  BEGIN CONTENT AREA  -->
+    <div id="content" class="main-content">
+        <div class="layout-px-spacing">
+            <div class="middle-content container-xxl p-0">
+                <div class="row layout-top-spacing">
+                    <div class="col-12">
+                        <slot/>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <Footer/>
+    </div>
+    <!--  END CONTENT AREA  -->
+</div>
+<!-- END MAIN CONTAINER -->
+
+
+
+<!--
+<style>
+    body.dark .layout-px-spacing, .layout-px-spacing {
+        min-height: calc(100vh - 155px) !important;
+    }
+</style>-->
